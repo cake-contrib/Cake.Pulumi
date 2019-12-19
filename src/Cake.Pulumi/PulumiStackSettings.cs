6a028e05ce1df92ec90f07c81063ade86040ce45
James Connor <megakid@gmail.com>
2019-12-19 00:03:48 +0000

    Update PulumiStackSettings.cs

--- src/Cake.Pulumi/PulumiStackSettings.cs
+++ src/Cake.Pulumi/PulumiStackSettings.cs
@@ -2,12 +2,5 @@ namespace Cake.Pulumi
 {
     public class PulumiStackSettings : PulumiSettings
     {
-        public enum EnvCommandType
-        {
-            Workspace,
-            Env
-        }
-
-        public EnvCommandType EnvCommand { get; set; } = EnvCommandType.Workspace;
     }
 }
